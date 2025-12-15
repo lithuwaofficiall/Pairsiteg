55fafcfe9e781cdc84b39faad9ac18f20c130723
Lithuwaofficial <gaminglithu8@gmail.com>
2025-12-15 11:42:52 +0530

    mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'lithikahesara@gmail.com',
-    password: '2008Lithika',
+    email: 'lithuhesara@gmail.com',
+    password: '._fM:VxBDW7m92V',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
